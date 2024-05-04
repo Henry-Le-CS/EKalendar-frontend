39c826bbed6d0a924454afa5fe3306962b78a318
Henry Le <lehieu2077@gmail.com>
2024-05-05 02:31:37 +0700

    style: add padding

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex items-center justify-center h-[100vh] border border-1 w-[100%]"
+    class="flex items-center px-4 justify-center h-[100vh] border border-1 w-[100%]"
   >
     <div
       class="h-max flex items-center justify-center w-full px-4 py-8 lg:w-[50%] border border-1 rounded-md"
