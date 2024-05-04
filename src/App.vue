724a57cb9cad391fa332f0617090a5c9df11985f
Henry Le <lehieu2077@gmail.com>
2024-05-05 02:41:41 +0700

    feat: add title for website

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div
-    class="flex items-center px-4 justify-center h-[100vh] border border-1 w-[100%]"
+    class="flex flex-col items-center gap-4 px-4 justify-center h-[100vh] border border-1 w-[100%]"
   >
+    <div class="font-mono font-thin text-[#006DCC]">
+      <span class="text-6xl text-[#005f69]">E</span>
+      <span class="text-6xl">K</span>
+      <span class="text-6xl">alendar</span>
+    </div>
     <div
       class="h-max flex items-center justify-center w-full px-4 py-8 lg:w-[50%] border border-1 rounded-md"
     >
